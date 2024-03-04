28781bc27059162b133d69c92d40b0a71b643437
Clark Alesna <clark_alesna@hotmail.com>
2024-03-04 21:11:06 +0800

    client_data needs to be boxed back after using

--- src/pallas-dotnet-rs/src/lib.rs
+++ src/pallas-dotnet-rs/src/lib.rs
@@ -11,11 +11,7 @@ use pallas::{
     network::{
         facades::{Error, NodeClient},
         miniprotocols::{
-            chainsync,
-            localstate::queries_v16,
-            localtxsubmission::{EraTx, GenericClient, RejectReason},
-            Point as PallasPoint, MAINNET_MAGIC, PREVIEW_MAGIC, PRE_PRODUCTION_MAGIC,
-            PROTOCOL_N2C_TX_SUBMISSION, TESTNET_MAGIC,
+            chainsync, localstate::queries_v16, localtxsubmission::{EraTx, GenericClient, RejectReason}, Point as PallasPoint, MAINNET_MAGIC, PREVIEW_MAGIC, PRE_PRODUCTION_MAGIC, PROTOCOL_N2C_TX_SUBMISSION, PROTOCOL_N2N_TX_SUBMISSION, TESTNET_MAGIC
         },
         multiplexer::{self, Bearer},
     },
@@ -129,10 +125,10 @@ pub struct NodeClientWrapper {
 impl NodeClientWrapper {
     #[net]
     pub fn connect(socket_path: String, network_magic: u64) -> NodeClientWrapper {
-        NodeClientWrapper::connect_native(socket_path, network_magic)
+        NodeClientWrapper::connect(socket_path, network_magic)
     }
 
-    pub fn connect_native(socket_path: String, network_magic: u64) -> NodeClientWrapper {
+    pub fn connect(socket_path: String, network_magic: u64) -> NodeClientWrapper {
         let client = RT.block_on(async {
             NodeClient::connect(&socket_path, network_magic)
                 .await
@@ -175,6 +171,7 @@ impl NodeClientWrapper {
 
             // Convert client back to a raw pointer for future use
             let _ = Box::into_raw(client);
+            let _ = Box::into_raw(client_data);
 
             match tip {
                 PallasPoint::Origin => Point {
@@ -188,10 +185,10 @@ impl NodeClientWrapper {
 
     #[net]
     pub fn find_intersect(client_wrapper: NodeClientWrapper, known_point: Point) -> Option<Point> {
-        NodeClientWrapper::find_intersect_native(client_wrapper, known_point)
+        NodeClientWrapper::find_intersect(client_wrapper, known_point)
     }
 
-    pub fn find_intersect_native(
+    pub fn find_intersect(
         client_wrapper: NodeClientWrapper,
         known_point: Point,
     ) -> Option<Point> {
@@ -213,6 +210,7 @@ impl NodeClientWrapper {
 
             // Convert client back to a raw pointer for future use
             let _ = Box::into_raw(_client);
+            let _ = Box::into_raw(client_data);
 
             // Match on the intersecting point
             intersect_point.map(|pallas_point| match pallas_point {
@@ -238,9 +236,6 @@ impl NodeClientWrapper {
 
             // Get the next block
             let result = RT.block_on(async {
-                while !client.chainsync().has_agency() {
-                    client.chainsync().recv_while_must_reply().await.unwrap();
-                }
                 client.chainsync().request_next().await
             });
 
@@ -377,7 +372,9 @@ impl NodeClientWrapper {
             };
 
             // Convert client back to a raw pointer for future use
+
             let _ = Box::into_raw(client);
+            let _ = Box::into_raw(client_data);
             next_response
         }
     }
@@ -397,12 +394,17 @@ impl NodeClientWrapper {
 
             // Convert client back to a raw pointer for future use
             let _ = Box::into_raw(_client);
+            let _ = Box::into_raw(client_data);
 
             has_agency
         }
     }
 
     #[net]
+    pub fn submit_tx(client_wrapper: NodeClientWrapper, tx: Vec<u8>) -> bool {
+        NodeClientWrapper::submit_tx(client_wrapper, tx)
+    }
+
     pub fn submit_tx(client_wrapper: NodeClientWrapper, tx: Vec<u8>) -> bool {
         unsafe {
             let client_data_ptr = client_wrapper.client_data_ptr as *mut NodeClientWrapperData;
@@ -413,13 +415,13 @@ impl NodeClientWrapper {
             // Convert the raw pointer back to a Box to deallocate the memory
             let mut _client = Box::from_raw(client_ptr);
             RT.block_on(async {
-                let bearer = Bearer::connect_unix(client_data.socket_path)
+                let bearer = Bearer::connect_unix(&client_data.socket_path)
                     .await
                     .map_err(Error::ConnectFailure)
                     .unwrap();
 
                 let mut plexer = multiplexer::Plexer::new(bearer);
-                let txsubmit_channel = plexer.subscribe_client(PROTOCOL_N2C_TX_SUBMISSION);
+                let txsubmit_channel = plexer.subscribe_client(PROTOCOL_N2N_TX_SUBMISSION);
                 let mut txsubmit_client: GenericClient<EraTx, RejectReason> =
                     GenericClient::new(txsubmit_channel);
                 let decoded_tx = MultiEraTx::decode(&tx).unwrap();
@@ -429,6 +431,7 @@ impl NodeClientWrapper {
 
             // Convert client back to a raw pointer for future use
             let _ = Box::into_raw(_client);
+            let _ = Box::into_raw(client_data);
 
             true
         }
@@ -499,11 +502,11 @@ mod tests {
     #[test]
     fn test_find_intersect() {
         let client_wrapper =
-            NodeClientWrapper::connect_native("/tmp/node.socket".to_string(), PREVIEW_MAGIC);
+            NodeClientWrapper::connect("/tmp/node.socket".to_string(), PREVIEW_MAGIC);
         let block_hash =
             hex::decode("a9e99c93352f91233a61fb55da83a43c49abf1c84a636e226e11be5ac0343dc3")
                 .unwrap();
-        let intersect = NodeClientWrapper::find_intersect_native(
+        let intersect = NodeClientWrapper::find_intersect(
             client_wrapper,
             Point {
                 slot: 35197575,
@@ -513,5 +516,14 @@ mod tests {
         .unwrap();
         assert_eq!(intersect.slot, 35197575);
         assert_eq!(intersect.hash, block_hash);
+    }
+
+    #[test]
+    fn test_tx_submit() {
+        let client_wrapper =
+            NodeClientWrapper::connect("/tmp/node.socket".to_string(), PREVIEW_MAGIC);
+        let tx = hex::decode("84A300828258207833C101F6A0F80F8BFDD6D90184CAB2A551CE9A595C0D4635E403FFDC31EA55018258207833C101F6A0F80F8BFDD6D90184CAB2A551CE9A595C0D4635E403FFDC31EA55020183A300581D7035FEB70954E97F2601B84C734D2A5315EE0A5E321E4B4F0E489C4CB201821A004C4B40A1581C8B05E87A51C1D4A0FA888D2BB14DBC25E8C343EA379A171B63AA84A0A144434E43541A000186A0028201D81858CBD8799FD8799F581CE63022B0F461602484968BB10FD8F872787B862ACE2D7E943292A370FFD8799FD8799FD8799F581CE63022B0F461602484968BB10FD8F872787B862ACE2D7E943292A370FFD8799FD8799FD8799F581C03EC6A12860EF8C07D4C1A8DE7DF06ACB0F0330A6087ECBE972082A7FFFFFFFFD8799FFFFF1A000493E0D8799F051864FF581C8B05E87A51C1D4A0FA888D2BB14DBC25E8C343EA379A171B63AA84A044434E4354581C5496B3318F8CA933BBFDF19B8FAA7F948D044208E0278D62C24EE73EFF82583900E63022B0F461602484968BB10FD8F872787B862ACE2D7E943292A37003EC6A12860EF8C07D4C1A8DE7DF06ACB0F0330A6087ECBE972082A7821A0011A008A1581C8B05E87A51C1D4A0FA888D2BB14DBC25E8C343EA379A171B63AA84A0A144434E43541A00030D4082583900E63022B0F461602484968BB10FD8F872787B862ACE2D7E943292A37003EC6A12860EF8C07D4C1A8DE7DF06ACB0F0330A6087ECBE972082A71B00000004A345246C021A0002D039A100818258206395DDF277DCCA535AAB1C5C65680E8C73AAE3AB883060EF88C45A2DDE1287325840653EB36C0C6019599F7E8376BC458C5C8774BEC66BE951D09BC4009753C58A297E6D8EA5318CBFEC6F1C11E2A88F234B2F467E64C7D089D350E5CFCCA9E56401F5F6");
+        let result = NodeClientWrapper::submit_tx(client_wrapper, tx.unwrap());
+        assert!(result);
     }
 }
