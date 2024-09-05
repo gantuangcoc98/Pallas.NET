1a946d3e83c6885e9bc429539029a9abdff88bdc
Christian Benedict R. Gantuangco <91376403+gantuangcoc98@users.noreply.github.com>
2024-09-06 03:33:36 +0800

    Update build-rs.sh

--- src/pallas-dotnet/build-rs.sh
+++ src/pallas-dotnet/build-rs.sh
@@ -2,16 +2,16 @@
 
 # Function to build and copy for Linux
 build_for_linux() {
-    cargo build --release --manifest-path ./src/pallas-dotnet-rs/Cargo.toml
-    cp ./src/pallas-dotnet-rs/target/release/libpallas_dotnet_rs.so "./src/pallas-dotnet/libpallas_dotnet_rs.so"
-    rnet-gen ./src/pallas-dotnet-rs/target/release/libpallas_dotnet_rs.so > "./src/pallas-dotnet/PallasDotnetWrapper.cs"
+    cargo build --release --manifest-path ../pallas-dotnet-rs/Cargo.toml
+    cp ../pallas-dotnet-rs/target/release/libpallas_dotnet_rs.so "./libpallas_dotnet_rs.so"
+    rnet-gen ../pallas-dotnet-rs/target/release/libpallas_dotnet_rs.so > "./PallasDotnetWrapper.cs"
 }
 
 # Function to build and copy for macOS
 build_for_macos() {
-    cargo build --release --manifest-path ./src/pallas-dotnet-rs/Cargo.toml
-    cp ./src/pallas-dotnet-rs/target/release/libpallas_dotnet_rs.dylib "./src/pallas-dotnet/libpallas_dotnet_rs.dylib"
-    rnet-gen ../pallas-dotnet-rs/target/release/libpallas_dotnet_rs.dylib > "./src/pallas-dotnet/PallasDotnetWrapper.cs"
+    cargo build --release --manifest-path ../pallas-dotnet-rs/Cargo.toml
+    cp ../pallas-dotnet-rs/target/release/libpallas_dotnet_rs.dylib "./libpallas_dotnet_rs.dylib"
+    rnet-gen ../pallas-dotnet-rs/target/release/libpallas_dotnet_rs.dylib > "./PallasDotnetWrapper.cs"
 }
 
 # Check the operating system
