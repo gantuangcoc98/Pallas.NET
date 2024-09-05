160ae1f120fdbcc452dea7691278f275c4816e60
Christian Benedict R. Gantuangco <91376403+gantuangcoc98@users.noreply.github.com>
2024-09-06 03:04:04 +0800

    Create build-rs.sh

---
+++ src/pallas-dotnet/build-rs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Function to build and copy for Linux
+build_for_linux() {
+    cargo build --release --manifest-path ./src/pallas-dotnet-rs/Cargo.toml
+    cp ./src/pallas-dotnet-rs/target/release/libpallas_dotnet_rs.so "./src/pallas-dotnet/libpallas_dotnet_rs.so"
+    rnet-gen ./src/pallas-dotnet-rs/target/release/libpallas_dotnet_rs.so > "./src/pallas-dotnet/PallasDotnetWrapper.cs"
+}
+
+# Function to build and copy for macOS
+build_for_macos() {
+    cargo build --release --manifest-path ./src/pallas-dotnet-rs/Cargo.toml
+    cp ./src/pallas-dotnet-rs/target/release/libpallas_dotnet_rs.dylib "./src/pallas-dotnet/libpallas_dotnet_rs.dylib"
+    rnet-gen ../pallas-dotnet-rs/target/release/libpallas_dotnet_rs.dylib > "./src/pallas-dotnet/PallasDotnetWrapper.cs"
+}
+
+# Check the operating system
+OS="`uname`"
+case $OS in
+  'Linux')
+    # Linux-specific commands
+    build_for_linux
+    ;;
+  'Darwin')
+    # macOS-specific commands
+    build_for_macos
+    ;;
+  *) ;;
+esac
